4ca5391f2493c5e34da77d328b8b15ebfaa30229
TatianAnat <TatianAnat@yandex.ru>
2025-05-09 16:28:29 +0400

    homework 14 task 7

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,10 @@ public class Main {
             Author author2 = new Author("Владимир", "Набоков");
             Book book1 = new Book("Доктор Живаго", author1, 1957);
             Book book2 = new Book("Дар", author2, 1938);
+            System.out.println("Книга " + book1);
+            System.out.println("Книга " + book2);
+            book1.setDate(2024);
+            System.out.println("Книга " + book1);
 
         }
         public class Book {
