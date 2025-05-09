cb9197fac2084e1f3a6d1a36653fb2104584a8aa
TatianAnat <TatianAnat@yandex.ru>
2025-05-09 15:58:41 +0400

    homework 14 task 6

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,37 @@
 public class Main {
+    public static void main(String[] args) {
 
-      public static void main(String[] args) {
 
     }
+    public class App{
+        public static void main(String[] args) {
+            Author author1 = new Author("Борис", "Пастернак");
+            Author author2 = new Author("Владимир", "Набоков");
+            Book book1 = new Book("Доктор Живаго", author1, 1957);
+            Book book2 = new Book("Дар", author2, 1938);
 
-    public class Book {
-        String nameBook;
-        Author author;
-        int date;
-        public Book(String nameBook, Author author, int date) {
-            this.nameBook = nameBook;
-            this.author = author;
-            this.date = date;
+        }
+        public class Book {
+            String nameBook;
+            Author author;
+            int date;
+            public Book(String nameBook, Author author, int date) {
+                this.nameBook = nameBook;
+                this.author = author;
+                this.date = date;
+            }
+
+        }
+
+        public class Author {
+            String nameAuthor;
+            String surname;
+            public Author(String nameAuthor, String surname) {
+                this.nameAuthor = nameAuthor;
+                this.surname = surname;
+            }
         }
 
     }
 
-    public class Author {
-        String nameAuthor;
-        String surname;
-        public Author(String nameAuthor, String surname) {
-            this.nameAuthor = nameAuthor;
-            this.surname = surname;
-        }
-
     }
-}
