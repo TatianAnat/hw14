3faba06b3098fcd0c84ad5e6b0d46d398665dfcc
TatianAnat <TatianAnat@yandex.ru>
2025-05-09 17:00:17 +0400

    homework 14 task 7

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,19 @@
 public class Main {
+    //public void main(String[] args) {
+    // }
     public static void main(String[] args) {
 
-
+        Author author1 = new Author("Борис ", "Пастернак");
+        Author author2 = new Author("Владимир ", "Набоков");
+        Book book1 = new Book("Доктор Живаго ", author1, 1957);
+        Book book2 = new Book("Дар ", author2, 1938);
+        System.out.println("Книга " + book1.getNameBook() + "Автор " + author1.getNameAuthor());
+        System.out.println("Книга " + book2.getNameBook() + "Автор " + author2.getNameAuthor());
+        book1.setDate(2024);
+        System.out.println("Книга " + book1);
     }
-    public class App{
-        public static void main(String[] args) {
-            Author author1 = new Author("Борис", "Пастернак");
-            Author author2 = new Author("Владимир", "Набоков");
-            Book book1 = new Book("Доктор Живаго", author1, 1957);
-            Book book2 = new Book("Дар", author2, 1938);
-            System.out.println("Книга " + book1.getNameBook() + "Автор " + author1.getNameAuthor());
-            System.out.println("Книга " + book2.getNameBook() + "Автор " + author2.getNameAuthor());
-            book1.setDate(2024);
-            System.out.println("Книга " + book1);
+}
 
-        }
-        public class Book {
-            private String nameBook;
-            private Author author;
-            int date;
-            public Book(String nameBook, Author author, int date) {
-                this.nameBook = nameBook;
-                this.author = author;
-                this.date = date;
-            }
-            public String  getNameBook() {
-                return this.nameBook;
-            }
-            public int getDate() {
-                return this.date;
-            }
-            public void setDate(int date) {
-                this.date = date;
-            }
-        }
 
-        public class Author {
-            private String nameAuthor;
-            private String surname;
-            public Author(String nameAuthor, String surname) {
-                this.nameAuthor = nameAuthor;
-                this.surname = surname;
-            }
-            public String getNameAuthor() {
-                return this.nameAuthor;
-            }
-            public String getSurname() {
-                return this.surname;
-            }
-        }
 
-    }
 
-    }
