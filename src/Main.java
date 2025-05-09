e7c5e15c4c87e09572998f02a4e688a434680723
TatianAnat <TatianAnat@yandex.ru>
2025-05-09 15:19:39 +0400

    homework 14 task 3

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,6 @@
 public class Main {
-    public static Book createBook(String nameBook, Author author, int date){
-       Book book = new Book();
-       book.nameBook = nameBook;
-       book.date = date;
-       return book;
-    }
-    public static Author createAuthor(String nameAuthor, String surname){
-        Author author = new Author();
-        author.nameAuthor = nameAuthor;
-        author.surname = surname;
-        return author;
-    }
-    public static void main(String[] args) {
+
+      public static void main(String[] args) {
 
     }
 
@@ -19,13 +8,21 @@ public class Main {
         String nameBook;
         Author author;
         int date;
-
+        public Book(String nameBook, Author author, int date) {
+            this.nameBook = nameBook;
+            this.author = author;
+            this.date = date;
+        }
 
     }
 
     public class Author {
         String nameAuthor;
         String surname;
+        public Author(String nameAuthor, String surname) {
+            this.nameAuthor = nameAuthor;
+            this.surname = surname;
+        }
 
     }
 }
